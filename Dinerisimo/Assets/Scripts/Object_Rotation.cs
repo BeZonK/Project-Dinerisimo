4a680835ce4fc13c31bde44ed2ffb79fe957be1c
PONDERATO <jonas.carasa@gmail.com>
2022-10-14 12:17:33 +0200

    Inspector de modelos funcional

--- Dinerisimo/Assets/Scripts/Object_Rotation.cs
+++ Dinerisimo/Assets/Scripts/Object_Rotation.cs
@@ -1,29 +1,36 @@
-
 using UnityEngine;
-
 
 public class Object_Rotation : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float rotationSpeed = 5f;
     private bool isDragging = false;                    //Necessary so the object doesn't move just mantaining the mouse button clicked.
+
+    private void Update()
+    {
+        //Reset the object rotation to 0.
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.rotation = Quaternion.identity;
+        }
+    }
 
     private void OnMouseDrag()
     {
-        //First we check if tis moving (horizontally or vertically) 
+        //First we check if its moving (horizontally or vertically) or not
         if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
         {
             isDragging = true;
         }
-
-        //If there's no movement
         else
         {
             isDragging = false;
         }
 
+        //If there's movement, we rotate the object accordingly.
         if (isDragging)
         {
-            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0) * rotationSpeed);
+            //In order to rotate in the way we want, it has to be in WORLD COORDINATES instead of local ones.
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0) * rotationSpeed, Space.World);
         }
     }
 
